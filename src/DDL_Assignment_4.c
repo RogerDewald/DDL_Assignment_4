a93f340d482b5ee67731574042511919a3af1074
Daniel Roger Dewald <123123188+RogerDewald@users.noreply.github.com>
2025-03-28 16:11:25 -0500

    Add files via upload

--- src/DDL_Assignment_4.c
+++ src/DDL_Assignment_4.c
@@ -58,7 +58,8 @@
 volatile int units = 1;
 
 void Start0() {
-    I2C0CONSET = CONSET_STA;
+    I2C0CONSET = CONCLR_SIC;
+     I2C0CONSET = CONSET_STA;
     I2C0CONCLR = CONCLR_SIC;
     while (((I2C0CONSET >> 3) & 1) == 0) {
     }
@@ -123,20 +124,20 @@ int Read1(int ack) {
 }
 
 void Expander_Write(int reg, int data) {
-    Start1();
-    Write1(EXPANDER_ADDRESS | 0);
-    Write1(reg);
-    Write1(data);
-    Stop1();
+    Start0();
+    Write0(EXPANDER_ADDRESS | 0);
+    Write0(reg);
+    Write0(data);
+    Stop0();
 }
 
 int Expander_Read(int reg) {
     int data;
-    Start1();
-    Write1(EXPANDER_ADDRESS | 0);
-    Write1(reg);
-    Start1();
-    Write1(EXPANDER_ADDRESS | 1);
+    Start0();
+    Write0(EXPANDER_ADDRESS | 0);
+    Write0(reg);
+    Start0();
+    Write0(EXPANDER_ADDRESS | 1);
     data = Read1(0);
     Stop1();
     return data;
@@ -146,14 +147,14 @@ int Temp_Read_Cel(void) {
     int MSB;
     int LSB;
     Start0();
-    Write0(~(TEMP_ADDRESS | 0));
+    Write0(TEMP_ADDRESS | 0);
     Write0(0x00);
     Start0();
-    Write0(~(TEMP_ADDRESS | 1));
-    MSB = (Read0(1));
-    LSB = (Read0(0));
+    Write0(TEMP_ADDRESS | 1);
+    MSB = Read0(1);
+    LSB = Read0(0);
     Stop0();
-    return ((MSB << 1) | (LSB)) >> 7;
+    return ((MSB << 1) | (LSB >> 7));
     //return ((MSB << 3) | (LSB)) >> 5;
 }
 
@@ -174,9 +175,15 @@ void Initialization() {
     I2C1SCLH = 5;
     I2C1SCLL = 5;
 
+    I2C0CONSET = CONSET_SI;
+    I2C0CONCLR = CONCLR_STAC;
+    I2C0CONCLR = CONCLR_I2ENC;
     I2C0CONSET = CONSET_I2EN;
 
-    //Expander_Write(EXPANDER_IODIRA, 0x00);
+    //I2C1CONCLR = CONCLR_I2ENC;
+    //I2C1CONSET = CONSET_I2EN;
+
+    //Expander_Write(EXPANDER_IODIRA, 0x01);
     //Expander_Write(EXPANDER_IODIRB, 0x00);
 }
 
@@ -191,36 +198,29 @@ void Wait(float secs) {
     }
 }
 
-int BinaryDecimal(int n) {
-    int num = n;
-    int dec_value = 0;
-    int base = 1;
-    int temp = num;
-
-    while (temp) {
-        int last_digit = temp % 10;
-        temp = temp / 10;
-
-        dec_value += last_digit * base;
-
-        base = base * 2;
-    }
-
-    return dec_value;
-}
 int Switch1(int value) {
     int output = 0;
     switch (value) {
     case 0:
+    	return output | 0b01000001;
     case 1:
+    	return output | 0b01000001;
     case 2:
+    	return output | 0b10000011;
     case 3:
+    	return output | 0b10001001;
     case 4:
+    	return output | 0b00101101;
     case 5:
+    	return output | 0b00011001;
     case 6:
+    	return output | 0b00010001;
     case 7:
+    	return output | 0b11001101;
     case 8:
+    	return output | 0b00000001;
     case 9:
+    	return output | 0b00001001;
     default:
         return output;
     }
@@ -229,15 +229,25 @@ int Switch2(int value) {
     int output = 0;
     switch (value) {
     case 0:
+    	return output | 0b00000010;
     case 1:
+    	return output | 0b11001110;
     case 2:
+    	return output | 0b10000001;
     case 3:
+    	return output | 0b10001000;
     case 4:
+    	return output | 0b01001100;
     case 5:
+    	return output | 0b00101000;
     case 6:
+    	return output | 0b00100000;
     case 7:
+    	return output | 0b10001110;
     case 8:
+    	return output | 0b00000000;
     case 9:
+    	return output | 0b00001000;
     default:
         return output;
     }
@@ -249,11 +259,11 @@ void display_number(int value) {
     int output;
 
     for (int i = 0; i < 50; i++) {
-    	output = (Switch1(tens) | 240) | (Switch2(ones) | 15);
+    	output = (Switch1(tens) | 240) | (Switch2(ones) >> 4);
         Expander_Write(EXPANDER_GPIOA, output);
         Wait(0.01);
-    	output = (Switch2(ones) | 240) | (Switch1(tens) | 15);
-        Expander_Write(EXPANDER_GPIOA, output);
+    	output = ((Switch2(ones) | 15) << 4) | (Switch1(tens) | 15);
+        Expander_Write(EXPANDER_GPIOB, output);
         Wait(0.01);
     }
 }
@@ -262,21 +272,32 @@ int main(void) {
     Initialization();
     float temp_c;
     int temp_f;
-    int intermediate;
+    int far;
 
     while (1) {
-
-        temp_c = Temp_Read_Cel() * 0.125;
+        temp_c = (float) Temp_Read_Cel() * 0.5;
         temp_f = Convert(temp_c);
+
+    	//if(!(Expander_Read(EXPANDER_GPIOA) & 1) && units) {
+    	//	far = 0;
+        //	Wait(0.02);
+    	//}
+    	//else if(!(Expander_Read(EXPANDER_GPIOA) & 1) && !units) {
+    	//	far = 1;
+        //	Wait(0.02);
+    	//}
+
+
         if (temp_f > 99) {
             temp_f = 99;
         }
         if (temp_f < 0) {
             temp_f = 0;
         }
-        if (units) {
-            //display_number(temp_f);
+        if (far) {
+         //   display_number(temp_f);
         } else {
+        	//display_number(temp_c);
         }
         //Wait(1);
     }
